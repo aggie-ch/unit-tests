852ff844379d7831d5f06d7a2405fd4bcb95814a
aggie-ch <aggie-ch@github.com>
2020-11-10 12:23:11 +0100

    new Cart class & assertTimeout

---
+++ src/test/java/io/github/aggie/testing/CartTest.java
@@ -0,0 +1,19 @@
+package io.github.aggie.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+        // given
+        Cart cart = new Cart();
+
+        // when + then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
