f4650adcf40f336df6865689c635d889de8e7430
aggie-ch <aggie-ch@github.com>
2020-11-10 11:59:07 +0100

    @AfterAll & @BeforeAll

--- src/test/java/io/github/aggie/testing/OrderBackupTest.java
+++ src/test/java/io/github/aggie/testing/OrderBackupTest.java
@@ -1,16 +1,46 @@
 package io.github.aggie.testing;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 class OrderBackupTest {
 
     private static OrderBackup orderBackup;
 
     @BeforeAll
-    static void setup() {
+    static void setup() throws FileNotFoundException {
+        orderBackup = new OrderBackup();
+        orderBackup.createFile();
+    }
 
+    @BeforeEach
+    void appendAtTheStartOfTheLine() throws IOException {
+        orderBackup.getWriter().append("Order: ");
+    }
+
+    @AfterEach
+    void appendAtTheEndOfTheLine() throws IOException {
+        orderBackup.getWriter().append(" backed up.");
+    }
+
+    @Test
+    void backupOrderWithOneMeal() throws IOException {
+        // given
+        Meal meal = new Meal(11, "Sweet potatoes");
+        Order order = new Order();
+        order.addMealToOrder(meal);
+
+        // when
+        orderBackup.backupOrder(order);
+
+        // then
+        System.out.println(order.toString());
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        orderBackup.closeFile();
     }
 }
